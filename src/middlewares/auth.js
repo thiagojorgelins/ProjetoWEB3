1d6af894862ee2746c9a45c3838b0f2cab0376f9
Thiago Lins <thiago.jorge2001@gmail.com>
2024-03-28 01:21:11 -0300

    Add Auth

---
+++ src/middlewares/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken")
+
+function adminAcess(tipo) {
+    return async (req, res, next) => {
+        const authHeader = req.headers.authorization
+        if (!authHeader) {
+            return res.status(401).json({ msg: "Unauthorized" })
+        }
+
+        const [, token] = authHeader.split(' ')
+
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            console.log(decoded)
+            if (decoded.tipo !== tipo) {
+                return res.status(401).json({ error: 'Unauthorized' })
+            }
+            req.user = decoded
+            next()
+        } catch (error) {
+            return res.status(401).json({ msg: 'Invalid Token!' })
+        }
+
+    }
+}
+
+const adminAuth = adminAcess('Admin')
+
+module.exports = { adminAuth }
